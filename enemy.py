466364a2280fed1434c23807ca936aafcc72cbce
DanDaniel0 <Daniel@DanielAlhadeff.com>
2019-03-08 22:59:05 -0500

    SOnic

--- enemy.py
+++ enemy.py
@@ -146,3 +146,11 @@ class Bomb(Enemy): #Needs art, bomb
         self.hittable = False
         self.layer = FLOOR_DETAIL_LAYER
 
+class Hedgehog(Enemy): #Needs art, moves fast
+
+    def __init__(self, game, x, y):
+        Enemy.__init__(self, game, x, y, delay=0, behavior=ai.approach_player_smart_fast, hp = 2, damage=1)
+        idle = SpriteSheet("bit.png", (2, 1), 2)
+        self.sprite.add_animation({"Idle": idle})
+        self.sprite.start_animation("Idle")
+
